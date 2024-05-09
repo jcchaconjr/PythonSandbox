0955f7224705aa542a233e3dedea6b7e5cf231e3
Julio C. Chacon Jr. <j_c_chacon_jr@hotmail.com>
2024-05-09 18:07:12 -0400

    Updated RankedCities code
    - completed the logic to finish the assignment.
    - Imported itemgetter to simplify ht eprocess of sorting the list.

--- RankedCities.py
+++ RankedCities.py
@@ -1,3 +1,11 @@
+from operator import itemgetter
+
+def sortAndPrint(listToSort):
+    listToSort = sorted(listToSort, key=itemgetter('rank'))
+    for rankedCity in listToSort:
+        print(f"{rankedCity["rank"]}. {rankedCity["city"]}")
+
+
 rankedCities = [{"city":"Homestead", "rank": 1}, 
                 {"city":"Miami", "rank": 2},
                 {"city":"Doral", "rank": 3},
@@ -5,15 +13,15 @@ rankedCities = [{"city":"Homestead", "rank": 1},
                 {"city":"Coral Gables", "rank": 5},
                 {"city":"Fort Lauderdale", "rank": 6}]
 
+
 print("Here is the list of best cities in South Florida (in order):\n")
 
-for rankedCity in rankedCities:
-    print(f"{rankedCity["rank"]}. {rankedCity["city"]}")
+sortAndPrint(rankedCities)
 
 print("Disagree? Please enter a city to move:\n")
 cityToMove = str(input ("Which city would you like to reposition? "))
 
-selectedDict = dict()
+selectedDict = {"city": "none", "rank": 0}
 for cityDict in rankedCities:
     if cityDict["city"] == cityToMove:
         selectedDict = cityDict.copy()
@@ -22,13 +30,22 @@ if selectedDict["city"] == cityToMove:
     newPos = int(input (f"Where would you like to place {cityToMove}? "))
     if (newPos >= 0 and newPos <= len(rankedCities) - 1):
         if selectedDict["rank"] == newPos:
-            print(f"{cityToMove} is already in position {newPos}.")
+            print(f"{cityToMove} is already in position {newPos}.\n")
         else:
-            # Need to find the other one
-            cityList.remove(cityToMove) #this is old code
-            cityList.insert(newPos-1, cityToMove) # this is old code
+            for cityDict in rankedCities:
+                if cityDict["rank"] == newPos:
+                    swapDict = cityDict.copy()
+                    break
+            swapDict["rank"] = selectedDict["rank"]
+            selectedDict["rank"] = newPos
+            for rankedCity in rankedCities:
+                if rankedCity["city"] == swapDict["city"]:
+                    rankedCity["rank"] = swapDict["rank"]
+                if rankedCity["city"] == selectedDict["city"]:
+                    rankedCity["rank"] = selectedDict["rank"]  
     else:
-        print("Please select a value within the range of the list.")
+        print("Please select a ranked value in the list.")
 else:
     print(f"{cityToMove} was not found in the list!\n")
 
+sortAndPrint(rankedCities)
